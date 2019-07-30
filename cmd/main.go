ee857999fa37f10fb5b8e21abe1507be002ba121
IQXI <xxshramxx@yandex.ru>
2019-07-30 22:13:25 +0300

    some fix

--- cmd/main.go
+++ cmd/main.go
@@ -3,19 +3,11 @@ package main
 import (
 	cfg "basic_microservice/internal/config"
 	lg "basic_microservice/internal/logger"
-	"go.uber.org/zap"
 )
 
-var logger *zap.Logger
-var config map[string]interface{}
-
-func init() {
-	config = cfg.GetConfig()
-	logger = lg.GetLogger(config)
-
-}
-
 func main() {
+	logger := lg.GetLogger(cfg.GetConfig())
+
 	logger.Info("Info msg")
 	logger.Fatal("Fatal msg")
 }
